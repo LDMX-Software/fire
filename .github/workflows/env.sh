bb7055564f94be9d9e97c5315941a58383355f84
tomeichlersmith <eichl008@umn.edu>
2022-02-09 15:03:31 -0600

    update env script and use it for bench markingjob

--- .github/workflows/env.sh
+++ .github/workflows/env.sh
@@ -4,6 +4,12 @@
 # same behavior in local and remote setup
 export GITHUB_WORKFLOWS_PATH="$(dirname ${BASH_SOURCE[0]})"
 export FIRE_TEST_MODULE_PATH="$(realpath ${GITHUB_WORKFLOWS_PATH}/../../test/module)"
+export FIRE_INSTALL_PREFIX="$(realpath ${GITHUB_WORKFLOWS_PATH}/../../install)"
+
+export PATH=${PATH}:${FIRE_INSTALL_PREFIX}/bin
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${FIRE_INSTALL_PREFIX}/lib
+export PYTHONPATH=${PYTHONPATH}:${FIRE_INSTALL_PREFIX}/python
+export CMAKE_PREFIX_PATH=${FIRE_INSTALL_PREFIX}
 
 if [ ! -z $BENCH_OUTPUT_DIR ]; then
   mkdir -p ${BENCH_OUTPUT_DIR} || { rc=$?; echo "Unable to create output dir."; return $rc; }
@@ -31,7 +37,7 @@ set_output() {
 __time__() {
   local n_trials=$1
   for ((i = 0; i < n_trials; i++)); do
-    ldmx time -p fire ${@:2} || return $?
+    time -p fire ${@:2} || return $?
   done |& awk '/real/ { real = real + $2; nr++ }
     END { if (nr>0) printf("%f\n", real/nr); }'
 }
