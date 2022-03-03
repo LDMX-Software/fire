8ae35374ea54b6abebbb788d9befe7549ce31b87
tomeichlersmith <eichl008@umn.edu>
2022-03-03 09:52:37 -0600

    testing dir to generate files with ROOT-based framework

---
+++ test/rootgen/Module/src/Module/Event/Hit.cxx
@@ -0,0 +1,48 @@
+#include "Module/Event/Hit.h"
+
+ClassImp(bench::Hit);
+
+namespace bench {
+
+Hit::Hit() {}
+
+Hit::~Hit() {}
+
+void Hit::Print() const {
+  std::cout << "Hit { "
+            << "layerID: " << layerID_ << ", "
+            << "moduleID: " << moduleID_ << ", "
+            << "position: ( " << x_ << ", " << y_ << ", " << z_ << " ), "
+            << "energy: " << energy_ << ", "
+            << "time: " << time_ << ", "
+            << "momentum: ( " << px_ << ", " << py_ << ", " << pz_ << " )"
+            << " }" << std::endl;
+}
+
+void Hit::Clear() {
+  layerID_ = 0;
+  moduleID_ = 0;
+  time_ = 0;
+  px_ = 0;
+  py_ = 0;
+  pz_ = 0;
+  x_ = 0;
+  y_ = 0;
+  z_ = 0;
+  energy_ = 0;
+  trackID_ = -1;
+  pdgID_ = 0;
+}
+
+void Hit::setPosition(const float x, const float y, const float z) {
+  this->x_ = x;
+  this->y_ = y;
+  this->z_ = z;
+}
+
+void Hit::setMomentum(const float px, const float py, const float pz) {
+  this->px_ = px;
+  this->py_ = py;
+  this->pz_ = pz;
+}
+}  // namespace bench
---
+++ test/rootgen/Module/src/Module/Produce.cxx
@@ -0,0 +1,46 @@
+
+#include "Module/Event/Hit.h"
+#include "Framework/EventProcessor.h"
+
+#include <random>
+
+namespace bench {
+
+class Produce : public framework::Producer {
+  /// the random number generator, unseeded so it produces the same results each time
+  std::mt19937 rng;
+  /// the distribution of sizes
+  std::uniform_int_distribution<std::size_t> rand_size;
+  /// the distribution of values in the Hits vector
+  std::uniform_real_distribution<float> rand_float; 
+  /// the distribution of integer values
+  std::uniform_int_distribution<long int> rand_int;
+ public:
+  Produce(const std::string& n, framework::Process& p)
+    : framework::Producer(n,p),
+    rng{}, // this is where a seed for the RNG would be put
+    rand_size{1, 100},
+    rand_float{0.,100.},
+    rand_int{-100,100}
+  {}
+  ~Produce() = default;
+  void produce(framework::Event& event) final override {
+    std::vector<Hit> rand_data;
+    rand_data.resize(rand_size(rng));
+    for (Hit& val : rand_data) {
+      val.setLayerID(rand_int(rng));
+      val.setModuleID(rand_int(rng));
+      val.setTrackID(rand_int(rng));
+      val.setPdgID(rand_int(rng));
+      val.setPosition(rand_float(rng),rand_float(rng),rand_float(rng));
+      val.setEnergy(rand_float(rng));
+      val.setTime(rand_float(rng));
+      val.setMomentum(rand_float(rng),rand_float(rng),rand_float(rng));
+    }
+    event.add("randdata", rand_data);
+  }
+};  // Produce
+
+}
+
+DECLARE_PRODUCER_NS(bench,Produce);
