02fff6d541c0b0c601c909f0e0ba294d47cac234
tomeichlersmith <eichl008@umn.edu>
2022-03-18 11:41:32 -0500

    functional pytest of new legacy module

--- python/LDMX/Framework/test_legacy.py
+++ python/LDMX/Framework/test_legacy.py
@@ -16,9 +16,8 @@ def test_legacy() :
 
     p = ldmxcfg.Process('test')
     ldmxcfg.Process.addModule('ShouldWork')
-    p.output_file = fire.cfg.OutputFile('test.h5')
-    p.keep('.*')
-    p.drop('.*')
+    p.outputFiles = ['test.h5']
+    p.keep = ['keep .*', 'drop .*']
 
     assert p.conditions.providers[-1] == p.rnss()
     assert p.rnss().seedMode == 'run'
@@ -29,7 +28,7 @@ def test_legacy() :
     assert p.rnss().seed == 420
 
     # check auto registration of conditions providers
-    cp = fire.cfg.ConditionsProvider('Provides','test::Provider','CPModule')
+    cp = ldmxcfg.ConditionsObjectProvider('Provides','test::Provider','CPModule')
     assert p.conditions.providers[-1] == cp
     assert p.libraries[-1] == 'libCPModule.so'
 
@@ -37,46 +36,23 @@ def test_legacy() :
     for cp in p.conditions.providers :
         assert cp.tag_name == 'NewDefault'
 
-    proc = fire.cfg.Processor('test','TestPythonConf','TestModule')
+    proc = ldmxcfg.Producer('test','TestPythonConf','TestModule')
     # check auto registration
     assert p.libraries[-1] == 'libTestModule.so'
 
-    proc2 = fire.cfg.Processor('test2','Testing',library='/full/path/to/lib.so')
+    proc2 = ldmxcfg.Analyzer('test2','Testing',library='/full/path/to/lib.so')
     assert p.libraries[-1] == '/full/path/to/lib.so'
 
     # add proc to sequence to test printing later
     p.sequence = [ proc ]
 
-    p.storage.default(False)
-    with pytest.raises(Exception) :
-        p.storage.listen('not_a_processor_instance')
-    p.storage.listen(proc)
+    assert p.storage.default_keep
+    p.skimDefaultIsDrop()
+    assert not p.storage.default_keep
 
-    import re
-    assert re.match(p.storage.listening_rules[-1].processor,proc.name)
+    p.skimConsider('test2')
+    assert p.storage.listening_rules[0].processor == 'test2'
 
-    p.storage.listen_all()
-    assert re.match(p.storage.listening_rules[-1].processor,'literally_anything')
-    assert re.match(p.storage.listening_rules[-1].purpose  ,'literally_anything')
-
-    # set up some dummy files to test directory globbing
-    import os
-    test_input_d = 'test_input_crawl'
-    os.makedirs(test_input_d,exist_ok=True)
-    file_names = ['one.h5','two.h5','three.h5']
-    exclude = ['noop.root','listing.txt']
-    for fn in file_names+exclude :
-        open(f'{test_input_d}/{fn}','a').close()
-
-    p.inputDir(test_input_d)
-
-    # need to sort because file globbing doesn't order the same as our list necessarily
-    assert p.input_files.sort() == [
-            os.path.realpath(os.path.join(test_input_d,fn)) 
-            for fn in file_names].sort()
-
-    dummy_var = str(p)
-    dummy_var = p.parameterDump()
     with pytest.raises(Exception) :
         p2 = ldmxcfg.Process('CantCreateTwo')
 
