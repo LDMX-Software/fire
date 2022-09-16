b6213f2a70e3303b8466dcb8683bfdb277e71514
tomeichlersmith <eichl008@umn.edu>
2022-09-16 15:36:09 -0500

    move the getting started docs to its own file for future reference
    easier to point people in that direction when it has its own page

--- include/fire/io/Data.h
+++ include/fire/io/Data.h
@@ -20,211 +20,8 @@
  *
  * Isolation of lower-level interaction with HDF5 files is done here.
  * These classes should never be used directly by the end user except
- * for the example provided below.
- * 
- * The ability of fire to handle
- * the saving and loading of data to and from a file comes from this namespace.
- * fire is able to handle all so-called "atomic" types (types with 
- * [numeric limits](https://en.cppreference.com/w/cpp/types/numeric_limits)
- * defined and std::string, std::vector of, and std::map of these types.
- *
- * This accomodates a lot of workflows, but it doesn't accomodate everything.
- * In order to make fire even more flexible, there is a method of interfacing
- * this serialization procedure with a class that you define.
- *
- * Below is the `MyData` class declaration showing the minium structure 
- * necessary to interface with fire's serialization method.
- *
- * ```cpp
- * #include "fire/io/Data.h"
- * class MyData {
- *   friend class fire::io::Data<MyData>;
- *   MyData() = default;
- *   void clear();
- *   void attach(fire::io::Data<MyData>& d);
- * };
- * ```
- *
- * The user class has four necessary components:
- * 1. Your class declares the the wrapping io::Data class as a `friend`.
- *    - This allows the io::Data class access to the (potentially private)
- *      methods defined below.
- * 2. Your class has a (public or private) default constructor.
- *    - The default constructor may be how we initialize the data,
- *      so it must be defined and available to fire::io.
- *    - If you don't want other parts of the program using the default
- *      constructor, you can declare it `private`.
- * 3. Your class has a `void clear()` method defined which resets the object
- *    to an "empty" or "blank" state.
- *    - This is used by fire to reset the data at the end of each event.
- *    - Similar to the default constructor, this method can be public 
- *      or private.
- * 4. Your class implements a `void attach(fire::io::Data<MyData>& d)` method.
- *    - This method should be private since it should not be called by
- *      other parts of your code.
- *    - More detail below.
- *
- * ## The attach Method
- * This method is where you make the decision on which member variables of
- * your class should be stored to or read from data files and how those
- * variables are named. You do this using the fire::io::Data<DataType>::attach
- * method. This is best illustrated with an example.
- *
- * ```cpp
- * // member_one_ and member_two_ are members of MyData
- * void MyData::attach(fire::io::Data<MyData>& d) {
- *   d.attach("first_member", member_one_);
- *   d.attach("another_member", member_two_);
- * }
- * ```
- *
- * ### Important Comments
- * - The name of a variable on disk (the first argument) and the name
- *   of the variable in the class do not need to relate to each other;
- *   however, it is common to name them similarly so users of your data
- *   files aren't confused.
- * - The name of variables on disk cannot be the same in one `attach`
- *   method, but they can repeat across different classes (similar
- *   to member variables).
- * - Passing io::Data as reference (i.e. with the `&`) is necessary;
- *   otherwise, you would attach to a local copy and the real io::Data
- *   wouldn't be attached to anything.
- * - The members of MyData you pass to io::Data::attach can be any
- *   class that fire::io can handle. This includes the classes listed
- *   above or other classes you have defined following these rules.
- *
- * ## ROOT Reading
- * As a transitory feature, reading from ROOT files fire::io::root previously
- * produced by a ROOT-based serialization framework has been implemented.
- * In order to effectively read these ROOT files, the user must provide the
- * ROOT dictionaries for the classes that they wish to read. The method used
- * in the testing module in this repository is a good example of how to get
- * this done; that method involves three steps.
- *
- * ### Step 1: Add ROOT macros to your class
- * You must include the `TObject.h` header file in order to have access to
- * these macros. Then use `ClassDef(<class-name>,<version>)` in the header
- * within the class definition. Finally, use `ClassDef(<ns>::<class-name>);`
- * in the source file. This lines should be wrapped by preprocessor checks
- * so that users compiling your library _without_ ROOT can still compile it.
- * For example,
- * ```cpp
- * #include <fire/io/Data.h> // get fire_USE_ROOT definition
- * #ifdef fire_USE_ROOT
- * #include <TObject.h>
- * #endif
- * ```
- * **Note**: ROOT associates the data stored in member variables with the
- * name of that member variable. This means that ROOT will print warnings
- * or event error out if new member variables are added or if member variables
- * change names from when the file was written with ROOT.
- *
- * ### Step 2: Write a LinkDef file.
- * This file _must_ end in the string `LinkDef.h`. The prefix to this can
- * be anything that makes sense to you. The template link def is given below
- * with a few examples of how to list classes. This file should be alongside
- * any other headers in your dictionary.
- * ```cpp
- * // MyEventLinkDef.h
- * #ifdef __CINT__
- * 
- * #pragma link off all globals;
- * #pragma link off all classes;
- * #pragma link off all functions;
- * #pragma link C++ nestedclasses;
- * 
- * // always have to list my class
- * #pragma link C++ class myns::MyClass+;
- * // include if you want to read vectors of your class
- * #pragma link C++ class std::vector<myns::MyClass>+;
- * // include if you want maps of your class 
- * // (key could be anything in dictionary, not just int)
- * #pragma link C++ class std::map<int,myns::MyClass>+;
- * 
- * #endif
- * ```
- *
- * ### Step 3: CMake Nonsense
- * ROOT has written a CMake function that can be used to attach a dictionary
- * compilation to an existing CMake target. It is a bit finnicky, so be careful
- * when deviating from the template below.
- * ```cmake
- * find_package(fire REQUIRED 0.13.0)
- * add_library(MyEvent SHARED <list-source-files>)
- * target_link_libraries(MyEvent PUBLIC fire::io)
- * if(fire_USE_ROOT)
- *   target_include_directories(MyEvent PUBLIC
- *     "$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>"
- *     "$<INSTALL_INTERFACE:${CMAKE_INSTALL_PREFIX}/include>")
- *   root_generate_dictionary(MyEventDict
- *     <list-header-files>
- *     LINKDEF ${CMAKE_CURRENT_SOURCE_DIR}/include/MyEvent/MyEventLinkDef.h
- *     MODULE MyEvent)
- *   install(FILES ${CMAKE_CURRENT_BINARY_DIR}/libMyEVent_rdict.pcm DESTINATION lib)
- * endif()
- * ```
- * This will include the compilation of a ROOT event dictionary if fire was built
- * with ROOT available.
- *
- * ## Full Example
- * Putting all of these notes together, below is an example class
- * that will read/write the coordinate positions but won't 
- * record the variable that is calculated from them.
- *
- * This isn't an amazing example since `std::sqrt` is pretty fast,
- * but you can perhaps imagine a class that has a time-expensive
- * calculation that should only be done once per event but is also
- * redundant and so it shouldn't waste disk space.
- *
- * ```cpp
- * // Point.h
- * #include "fire/io/Data.h"
- * #ifdef fire_USE_ROOT
- * #include "TObject.h"
- * #endif
- * class Point {
- *   double x_,y_,z_,mag_;
- *   friend class fire::io::Data<Point>;
- *   Point() = default;
- *   void clear();
- *   void attach(fire::io::Data<Point>& d);
- *  public:
- *   Point(double x, double y, double z);
- *   double mag() const;
- * #ifdef fire_USE_ROOT
- *   ClassDef(Point,1);
- * #endif
- * };
- * 
- * // Point.cxx
- * #include "Point.h"
- *
- * void Point::clear() {
- *   x_ = 0.;
- *   y_ = 0.;
- *   z_ = 0.;
- *   mag_ = -1.;
- * }
- * void Point::attach(fire::io::Data<Point>& d);
- *   d.attach("x",x_);
- *   d.attach("y",y_);
- *   d.attach("z",z_);
- * }
- * Point::Point(double x, double y, double z)
- *   : x_{x}, y_{y}, z_{z}, mag_{-1.} {
- *     mag();
- *   }
- * double Point::mag() const {
- *   if (mag_ < 0.) {
- *     mag_ = std::sqrt(x_*x_+y_*y_+z_*z_);
- *   }
- *   return mag_;
- * }
- *
- * #ifdef fire_USE_ROOT
- * ClassImp(Point);
- * #endif
- * ```
+ * for the attaching-method of defining new serializable types as 
+ * described in [Getting Started](md_docs_getting_started.html).
  *
  * ## Access Pattern
  *
@@ -259,23 +56,28 @@ AbstractData<DataType>::AbstractData(const std::string& path, Reader* input_file
  *
  * More complete documentation is kept in the documentation
  * of the fire::io namespace; nevertheless, a short example
- * is kep here.
+ * is kept here.
  *
  * ```cpp
  * class MyData {
  *  public:
+ *   fire_class_version(2);
  *   MyData() = default; // required by serialization technique
  *   // other public members
  *  private:
  *   friend class fire::io::Data<MyData>;
  *   void attach(fire::io::Data<MyData>& set) {
  *     set.attach("my_double",my_double_);
+ *     if (set.version() < 2) set.rename("old","new",new_);
+ *     else set.attach("new",new_);
  *   }
  *   void clear() {
  *     my_double_ = -1; // reset to default value
+ *     new_ = 0;
  *   }
  *  private:
  *   double my_double_;
+ *   int new_;
  *   // this member doesn't appear in 'attach' so it won't end up on disk
  *   int i_wont_be_on_disk_;
  * };
