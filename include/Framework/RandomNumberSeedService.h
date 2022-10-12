fb23599c3881854c4e31fa4d4b0ef80af5778454
tomeichlersmith <eichl008@umn.edu>
2022-10-12 13:27:41 -0500

    need RNSS in old namespace

---
+++ include/Framework/RandomNumberSeedService.h
@@ -0,0 +1,10 @@
+#ifndef FRAMEWORK_RANDOMNUMBERSEEDSERVICE_H
+#define FRAMEWORK_RANDOMNUMBERSEEDSERVICE_H
+
+#include "fire/RandomNumberSeedService.h"
+
+namespace framework {
+using RandomNumberSeedService = fire::RandomNumberSeedService;
+}
+
+#endif
